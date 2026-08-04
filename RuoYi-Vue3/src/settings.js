d0b5241ba8aef4f14b908bc2f113c1b5947e4dfb
ebaneban888xyz <ebaneban888+github@gmail.com>
2026-08-04 13:44:23 +0800

    前端版权年份动态化：登录页与首页底部 Copyright 年份随自然年自动更新

--- RuoYi-Vue3/src/settings.js
+++ RuoYi-Vue3/src/settings.js
@@ -62,6 +62,6 @@ export default {
   /**
    * 底部版权文本内容
    */
-  footerContent: 'Copyright © 2026 VQMS. All Rights Reserved.'
+  footerContent: `Copyright © ${new Date().getFullYear()} VQMS. All Rights Reserved.`
 }
 
